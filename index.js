a69c6403a41b8ef05c853c16860e09bdd1209be5
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-29 16:46:45 -0400

    added code to load main prompts when connection is made. added prompts to select tasks with object of choices

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ const connection = mysql.createConnection({
 
   connection.connect(function (err) {
     if (err) throw err;
+    // add code to run main page prompts when connection is made.
+    mainPage();
   });
-  
+
+  // add main page promts for user to select task - choices.
+  function mainPage(){
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+          "View Employees",
+          "View Employees by Department",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Add Role",
+          "Exit"]
+            
+    })
+        
+  }
   
