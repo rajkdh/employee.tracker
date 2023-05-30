79dafb9230ae3aea1c5951a0a1c8b3034ae22772
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-30 09:54:40 -0400

    updated code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const inquirer = require("inquirer");
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    password:"",
     database: 'employees'
   });
 
@@ -31,7 +32,37 @@ const connection = mysql.createConnection({
           "Add department",
           "Exit"]
             
+    }).then(function ({ task }){
+        switch (task) {
+            case "View Employees":
+              viewEmployee();
+              break;
+        }
     })
         
   }
   
+  function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      mainPage();
+    });
+    // console.log(query.sql);
+  }
+//   mainPage();
