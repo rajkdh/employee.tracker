47a8526687e701610616347878f916923eed3fec
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-06-01 06:14:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Avtar5485161!1",
   database: "employeedatabase"
 });
 
